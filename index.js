b7e021ed03b6f2fee784831ee8ec5aaccd4e6624
ChrisChrome <christophercookman@gmail.com>
2024-03-02 19:22:36 -0700

    Add captcha to form (silly people)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const { verify } = require('hcaptcha');
 const app = express();
 const path = require('path');
 const fs = require('fs');
 const config = require("./config.json")
 const Discord = require("discord.js");
 const hook = new Discord.WebhookClient({ url: config.contact_webhook })
-
+const hCaptchaSecret = config.captcha_secret;
 
 // Set up express for static files
 app.use(express.static('static'));
@@ -15,31 +17,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/contact", (req, res) => {
-	const { name, email, message } = req.body;
-	hook.send({
-		"content": "@everyone",
-		"embeds": [
-			{
-				"title": "New message!",
-				"color": 0x00ff00,
-				"fields": [
-					{
-						"name": "Name",
-						"value": name
-					},
-					{
-						"name": "Email",
-						"value": email
-					},
-					{
-						"name": "Message",
-						"value": message
-					}
-				]
-			}
-		]
-	});
-	res.redirect("/contact/?success=true");
+	const token = req.body["h-captcha-response"];
+	verify(hCaptchaSecret, token).then((capData) => {
+		if (capData.success === false) return res.redirect("/contact/?success=false&error=captcha");
+		const { name, email, message } = req.body;
+		hook.send({
+			"content": "@everyone",
+			"embeds": [
+				{
+					"title": "New message!",
+					"color": 0x00ff00,
+					"fields": [
+						{
+							"name": "Name",
+							"value": name
+						},
+						{
+							"name": "Email",
+							"value": email
+						},
+						{
+							"name": "Message",
+							"value": message
+						},
+						{
+							"name": "IP Address",
+							"value": req.headers['x-forwarded-for'] || req.connection.remoteAddress
+						}
+					]
+				}
+			]
+		});
+		res.redirect("/contact/?success=true");
+	})
 });
 
 // Start the express server
